fe64a31a4327708652fcbb172047fcec116e78b2
Leo <icpc0928@gmail.com>
2020-03-07 16:00:39 +0800

    20200307 調變器simpleAdapter / ListView 額外：recyclerview

--- app/src/main/java/tw/org/iii/leo/leo03/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leo03/MainActivity.java
@@ -3,8 +3,11 @@ package tw.org.iii.leo.leo03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,6 +43,27 @@ public class MainActivity extends AppCompatActivity {
         simpleAdapter = new SimpleAdapter(this,data,R.layout.item,from , to);
         lv.setAdapter(simpleAdapter);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showOneItem(position);
+            }
+        });
+    }
+
+    private void showOneItem(int pos){
+        Toast.makeText(this,data.get(pos).get(from[0]),Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void addItem(View view){
+        HashMap<String, String> row0 = new HashMap<>();
+        row0.put(from[0], "value" + (int)(Math.random()*100+100));
+        row0.put(from[1], "newData" );
+        row0.put(from[2], "2020-03-07 10:20:30");
+        data.add(0,row0);
+        simpleAdapter.notifyDataSetChanged();
+        //通知調變器要改變資料惹
     }
 
 }
